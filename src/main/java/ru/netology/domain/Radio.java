6c97387847de64457b27c5de1e69fe0d56f2f9aa
Leiza111 <leiza-rad@yandx.ru>
2022-01-27 13:36:17 +0300

    version2

--- src/main/java/ru/netology/domain/Radio.java
+++ src/main/java/ru/netology/domain/Radio.java
@@ -4,23 +4,49 @@ public class Radio {
 
     private int currentRadioStation; //текущая радиостанция
     private int currentVolume;
+    private int numberStation = 10;
 
-    //++установка(set) канала радиостанции
-    public void setCurrentRadioStation(int currentRadioStation) {
-        if (currentRadioStation < 0) { //если текущ.станция меньше 0, то выходи
+    //конструкторы
+    public Radio() { //конструктор без параметров+
+    }
+
+    public Radio(int numberStation) { //конст.с с параметром+
+        this.numberStation = numberStation;
+    }
+
+
+    //get/set numberStation
+    public int getNumberStation() {
+        return numberStation;
+    }
+
+    public void setNumberStation(int numberStation) {
+        if (numberStation < 0) {
             return;
         }
-        if (currentRadioStation > 9) { //если текущ.станция больше 9, то выходи
+        this.numberStation = numberStation;
+    }
+
+    //get/set currentRadioStation
+    public int getCurrentRadioStation() { //отдает текущую станцию сам, не лезем сами
+        return currentRadioStation;
+    }
+
+    public void setCurrentRadioStation(int currentRadioStation) {
+        if (currentRadioStation > numberStation - 1) {
+            return;
+        }
+        if (currentRadioStation < 0) {
             return;
         }
         this.currentRadioStation = currentRadioStation; // если 0-9, то меняем память станцию
     }
 
+
     //кнопка next-следующая станция
     public void nextRadioStation() {
-
-        if (currentRadioStation < 9) {
-            currentRadioStation = currentRadioStation + 1;
+        if (currentRadioStation < numberStation - 1) {
+            currentRadioStation++;
         } else {
             currentRadioStation = 0;
         }
@@ -29,42 +55,41 @@ public class Radio {
     //++кнопка prev-предыдущая станция
     public void prevRadioStation() {
         if (currentRadioStation > 0) {
-            currentRadioStation = currentRadioStation - 1;
+            currentRadioStation--;
         } else {
-            currentRadioStation = 9;
+            currentRadioStation = numberStation - 1;
         }
     }
 
-    //++возврвщаем значение поля радиостанции
-    public int getCurrentRadioStation() { //отдает текущую станцию сам, не лезем сами
-        return currentRadioStation;
+
+    //get/set currentVolume
+    public int getCurrentVolume() {
+        return currentVolume; //отдает текущую станцию
     }
 
-
-    //++выставление уровня звука
     public void setCurrentVolume(int currentVolume) {
-        if (currentVolume < 0) { //если текущ.станция меньше 0, то выходи
+        if (currentVolume > 100) { //если текущ.станция больше 100, то выходи
             return;
         }
-        if (currentVolume > 10) { //если текущ.станция больше 9, то выходи
+        if (currentVolume < 0) { //если текущ.станция меньше 0, то выходи
             return;
         }
         this.currentVolume = currentVolume; // если 0-9, то меняем память станцию
     }
 
-    //MaxVolume
-    public void setToMaxVolume() {
-        currentVolume = 10;
+
+    //Увеличение громкости
+    public void increaseVolume() {
+        if (currentVolume < 100) {
+            currentVolume++;
+        }
     }
 
-    //MinVolume
-    public void setToMinVolume() {
-        currentVolume = 0;
-    }
-
-    ////++возврвщаем значение уровня звука
-    public int getCurrentVolume() {
-        return currentVolume; //отдает текущую станцию
+    //Уменьшение громкости
+    public void reducеVolume() {
+        if (currentVolume > 0) {
+        }
+        currentVolume--;
     }
 }
 
--- src/test/java/ru/netology/domain/RadioTest.java
+++ src/test/java/ru/netology/domain/RadioTest.java
@@ -1,89 +1,80 @@
 package ru.netology.domain;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
-class RadioTest {
-    Radio rad = new Radio();
+public class RadioTest {
 
-      @Test //выставление допустимой радиостанции
-    public void shouldCurrentRadioStation() {
+    //SetNumberStation
+    @Test //выставление количества радиостанций
+    public void shouldSetNumberStation() {
         Radio rad = new Radio(); //создали pадио c текущейРадиостанцией 0
-
-        rad.setCurrentRadioStation(15);//возьметЗначе-еИПоложитВ currentRadiostation, кот.находится в голове объекта-запомнить, что положили в память
-
-        int expected = 0; //ожидаемый
-        int actual = rad.getCurrentRadioStation(); //фактический
-
+        rad.setNumberStation(5);//возьметЗначе-еИПоложитВ currentRadiostation, кот.находится в голове объекта-запомнить, что положили в память
+        int expected = 5; //ожидаемый
+        int actual = rad.getNumberStation(); //фактический
         assertEquals(expected, actual);
     }
 
-    @Test //выставление радиостанции выше макс
-    public void shouldAboveTheMaxRadioStation() {
-        Radio rad = new Radio(); //создалиРадиоСТекущейРадиостанцией 0
-
-        rad.setCurrentRadioStation(20);
+    @Test //выставление количества радиостанций
+    public void shouldSetNumberStation0() {
+        Radio rad = new Radio(); //создали pадио c текущейРадиостанцией 0
+        rad.setNumberStation(0);//возьметЗначе-еИПоложитВ currentRadiostation, кот.находится в голове объекта-запомнить, что положили в память
         int expected = 0; //ожидаемый
-        int actual = rad.getCurrentRadioStation(); //фактический
-
+        int actual = rad.getNumberStation(); //фактический
         assertEquals(expected, actual);
     }
 
+    @Test //выставление количества радиостанций меньше 0
+    public void shouldSetNumberStationLessZero() {
+        Radio rad = new Radio(); //создали pадио c текущейРадиостанцией 0
+        rad.setNumberStation(-2);//возьметЗначе-еИПоложитВ currentRadiostation, кот.находится в голове объекта-запомнить, что положили в память
+        int expected = 10; //ожидаемый
+        int actual = rad.getNumberStation(); //фактический
+        assertEquals(expected, actual);
+    }
+
+    //setCurrentRadioStation
     @Test //выставление радиостанции ниже мин
     public void shouldBelowTheMinRadioStation() {
         Radio rad = new Radio(); //создалиРадиоСТекущейРадиостанцией 0
-
         rad.setCurrentRadioStation(-5);
-
         int expected = 0; //ожидаемый
         int actual = rad.getCurrentRadioStation(); //фактический
-
         assertEquals(expected, actual);
     }
 
     @Test //граничные значения -1
     public void shouldMinusOneRadioStation() {
         Radio rad = new Radio(); //создалиРадиоСТекущейРадиостанцией 0
-
         rad.setCurrentRadioStation(-1);
-
         int expected = 0; //ожидаемый
         int actual = rad.getCurrentRadioStation(); //фактический
-
         assertEquals(expected, actual);
     }
 
     @Test //граничные значения 0
     public void shouldZeroRadioStation() {
         Radio rad = new Radio(); //создалиРадиоСТекущ.Радиост 0
-
         rad.setCurrentRadioStation(0);
-
         int expected = 0; //ожидаемый
         int actual = rad.getCurrentRadioStation(); //фактический
-
         assertEquals(expected, actual);
     }
 
     @Test //граничные значения 1
     public void shouldOneRadioStation() {
         Radio rad = new Radio(); //создалиРадиоСТекущейРадиостанцией 0
-
         rad.setCurrentRadioStation(1);
-
         int expected = 1; //ожидаемый
         int actual = rad.getCurrentRadioStation(); //фактический
-
         assertEquals(expected, actual);
     }
 
     @Test //граничные значения 8
     public void shouldEightRadioStation() {
         Radio rad = new Radio(); //создалиРадиоСТекущейРадиостанцией 0
-
         rad.setCurrentRadioStation(8);
-
         int expected = 8; //ожидаемый
         int actual = rad.getCurrentRadioStation(); //фактический
 
@@ -93,47 +84,36 @@ class RadioTest {
     @Test //граничные значения 9
     public void shouldNineRadioStation() {
         Radio rad = new Radio(); //создалиРадиоСТекущейРадиостанцией 0
-
         rad.setCurrentRadioStation(9);
-
         int expected = 9; //ожидаемый
         int actual = rad.getCurrentRadioStation(); //фактический
-
         assertEquals(expected, actual);
     }
 
     @Test //граничные значения 10
     public void shouldTenRadioStation() {
         Radio rad = new Radio(); //создалиРадиоСТекущейРадиостанцией 0
-
         rad.setCurrentRadioStation(10);
-
         int expected = 0; //ожидаемый
         int actual = rad.getCurrentRadioStation(); //фактический
-
         assertEquals(expected, actual);
     }
 
     @Test //кнопка next1(2)
     public void shouldNextRadioStation() {
         Radio rad = new Radio(); //создалиРадиоСТекущейРадиостанцией 0
-
         rad.setCurrentRadioStation(1);
         rad.nextRadioStation();
-
         int expected = 2; //ожидаемый
         int actual = rad.getCurrentRadioStation(); //фактический
-
         assertEquals(expected, actual);
     }
 
     @Test //кнопка next3(4)
     public void shouldNextRadioStation3() {
         Radio rad = new Radio(); //создалиРадиоСТекущейРадиостанцией 0
-
         rad.setCurrentRadioStation(3);
         rad.nextRadioStation();
-
         int expected = 4; //ожидаемый
         int actual = rad.getCurrentRadioStation(); //фактический
 
@@ -143,35 +123,28 @@ class RadioTest {
     @Test //кнопка next0(1)
     public void shouldNextRadioStation0() {
         Radio rad = new Radio(); //создалиРадиоСТекущейРадиостанцией 0
-
         rad.setCurrentRadioStation(0);
         rad.nextRadioStation();
-
         int expected = 1; //ожидаемый
         int actual = rad.getCurrentRadioStation(); //фактический
-
         assertEquals(expected, actual);
     }
 
     @Test //кнопка next8(9)
     public void shouldNextRadioStation8() {
         Radio rad = new Radio(); //создалиРадиоСТекущейРадиостанцией 0
-
         rad.setCurrentRadioStation(8);
         rad.nextRadioStation();
-
         int expected = 9; //ожидаемый
         int actual = rad.getCurrentRadioStation(); //фактический
-
         assertEquals(expected, actual);
     }
+
     @Test //кнопка next9(0)
     public void shouldNextRadioStation9() {
         Radio rad = new Radio(); //создалиРадиоСТекущейРадиостанцией 0
-
         rad.setCurrentRadioStation(9);
         rad.nextRadioStation();
-
         int expected = 0; //ожидаемый
         int actual = rad.getCurrentRadioStation(); //фактический
 
@@ -181,139 +154,322 @@ class RadioTest {
     @Test //кнопка prev4(3)
     public void shouldPrevRadioStation4() {
         Radio rad = new Radio(); //создалиРадиоСТекущейРадиостанцией 0
-
         rad.setCurrentRadioStation(4);
         rad.prevRadioStation();
-
         int expected = 3; //ожидаемый
         int actual = rad.getCurrentRadioStation(); //фактический
-
         assertEquals(expected, actual);
     }
 
     @Test //кнопка prev0(9)
     public void shouldPrevRadioStation0() {
         Radio rad = new Radio(); //создалиРадиоСТекущейРадиостанцией 0
-
         rad.setCurrentRadioStation(0);
         rad.prevRadioStation();
-
         int expected = 9; //ожидаемый
         int actual = rad.getCurrentRadioStation(); //фактический
 
         assertEquals(expected, actual);
     }
 
-    @Test //выставление допустимого уровня звука
-    public void shouldCurrentVolume() {
-        Radio rad = new Radio();
-
-        rad.setCurrentVolume(30);
-
-        int expected = 0; //ожидаемый
-        int actual = rad.getCurrentRadioStation(); //фактический
-
-        assertEquals(expected, actual);
-    }
 
     @Test //выставление уровня звука выше макс
     public void shouldAboveTheMaxVolume() {
         Radio rad = new Radio(); //создалиРадиоСТекущейРадиостанцией 0
-
-        rad.setCurrentVolume(30);
-
+        rad.setCurrentVolume(150);
         int expected = 0; //ожидаемый
         int actual = rad.getCurrentVolume(); //фактический
-
         assertEquals(expected, actual);
+    }
 
-
+    @Test //выставление допустимого уровня звука
+    public void shouldCurrentVolume() {
+        Radio rad = new Radio(); //создалиРадиоСТекущейРадиостанцией 0
+        rad.setCurrentVolume(100);
+        int expected = 100; //ожидаемый
+        int actual = rad.getCurrentVolume(); //фактический
+        assertEquals(expected, actual);
     }
 
     @Test //выставление уровня звука ниже мин
     public void shouldBelowTheMinVolume() {
         Radio rad = new Radio(); //
-
         rad.setCurrentVolume(-5);
-
         int expected = 0; //ожидаемый
         int actual = rad.getCurrentVolume(); //фактический
-
         assertEquals(expected, actual);
     }
 
     @Test //граничные значения -1
     public void shouldMinusOneVolume() {
         Radio rad = new Radio(); //создалиРадиоСТекущейРадиостанцией 0
-
         rad.setCurrentVolume(-1);
-
         int expected = 0; //ожидаемый
         int actual = rad.getCurrentVolume(); //фактический
-
         assertEquals(expected, actual);
     }
 
     @Test //граничные значения 0
     public void shouldZeroVolume() {
         Radio rad = new Radio(); //создалиРадиоСТекущейРадиостанцией 0
-
         rad.setCurrentVolume(0);
-
         int expected = 0; //ожидаемый
         int actual = rad.getCurrentVolume(); //фактический
-
         assertEquals(expected, actual);
     }
 
     @Test //граничные значения 1
     public void shouldOneVolum() {
         Radio rad = new Radio(); //создалиРадиоСТекущейРадиостанцией 0
-
         rad.setCurrentVolume(1);
-
         int expected = 1; //ожидаемый
-
         int actual = rad.getCurrentVolume(); //фактический
+        assertEquals(expected, actual);
+    }
+
+    @Test //граничные значения 99
+    public void shouldNineVolum() {
+        Radio rad = new Radio(); //создалиРадиоСТекущейРадиостанцией 0
+        rad.setCurrentVolume(99);
+        int expected = 99; //ожидаемый
+        int actual = rad.getCurrentVolume(); //фактический
+        assertEquals(expected, actual);
+    }
+
+
+
+
+
+
+
+    //SetNumberStation
+    @Test //выставление количества радиостанций
+    public void shouldSetNumberStationArgs() {
+        Radio rad = new Radio(30); //создали pадио c текущейРадиостанцией 0
+        rad.setNumberStation(5);//возьметЗначе-еИПоложитВ currentRadiostation, кот.находится в голове объекта-запомнить, что положили в память
+        int expected = 5; //ожидаемый
+        int actual = rad.getNumberStation(); //фактический
+        assertEquals(expected, actual);
+    }
+
+    @Test //выставление количества радиостанций
+    public void shouldSetNumberStation0Args() {
+        Radio rad = new Radio(30); //создали pадио c текущейРадиостанцией 0
+        rad.setNumberStation(0);//возьметЗначе-еИПоложитВ currentRadiostation, кот.находится в голове объекта-запомнить, что положили в память
+        int expected = 0; //ожидаемый
+        int actual = rad.getNumberStation(); //фактический
+        assertEquals(expected, actual);
+    }
+
+    @Test //выставление количества радиостанций меньше 0
+    public void shouldSetNumberStationLessZeroArgs() {
+        Radio rad = new Radio(30); //создали pадио c текущейРадиостанцией 0
+        rad.setNumberStation(-2);//возьметЗначе-еИПоложитВ currentRadiostation, кот.находится в голове объекта-запомнить, что положили в память
+        int expected = 30; //ожидаемый
+        int actual = rad.getNumberStation(); //фактический
+        assertEquals(expected, actual);
+    }
+
+    //setCurrentRadioStation
+    @Test //выставление радиостанции ниже мин
+    public void shouldBelowTheMinRadioStationArgs() {
+        Radio rad = new Radio(30); //создалиРадиоСТекущейРадиостанцией 0
+        rad.setCurrentRadioStation(-5);
+        int expected = 0; //ожидаемый
+        int actual = rad.getCurrentRadioStation(); //фактический
+        assertEquals(expected, actual);
+    }
+
+    @Test //граничные значения -1
+    public void shouldMinusOneRadioStationArgs() {
+        Radio rad = new Radio(30); //создалиРадиоСТекущейРадиостанцией 0
+        rad.setCurrentRadioStation(-1);
+        int expected = 0; //ожидаемый
+        int actual = rad.getCurrentRadioStation(); //фактический
+        assertEquals(expected, actual);
+    }
+
+    @Test //граничные значения 0
+    public void shouldZeroRadioStationArgs() {
+        Radio rad = new Radio(30); //создалиРадиоСТекущ.Радиост 0
+        rad.setCurrentRadioStation(0);
+        int expected = 0; //ожидаемый
+        int actual = rad.getCurrentRadioStation(); //фактический
+        assertEquals(expected, actual);
+    }
+
+    @Test //граничные значения 1
+    public void shouldOneRadioStationArgs() {
+        Radio rad = new Radio(30); //создалиРадиоСТекущейРадиостанцией 0
+        rad.setCurrentRadioStation(1);
+        int expected = 1; //ожидаемый
+        int actual = rad.getCurrentRadioStation(); //фактический
+        assertEquals(expected, actual);
+    }
+
+    @Test //граничные значения 8
+    public void shouldEightRadioStationArgs() {
+        Radio rad = new Radio(30); //создалиРадиоСТекущейРадиостанцией 0
+        rad.setCurrentRadioStation(8);
+        int expected = 8; //ожидаемый
+        int actual = rad.getCurrentRadioStation(); //фактический
 
         assertEquals(expected, actual);
     }
 
     @Test //граничные значения 9
-    public void shouldNineVolum() {
-        Radio rad = new Radio(); //создалиРадиоСТекущейРадиостанцией 0
-
-        rad.setCurrentVolume(9);
-
+    public void shouldNineRadioStationArgs() {
+        Radio rad = new Radio(30); //создалиРадиоСТекущейРадиостанцией 0
+        rad.setCurrentRadioStation(9);
         int expected = 9; //ожидаемый
-        int actual = rad.getCurrentVolume(); //фактический
+        int actual = rad.getCurrentRadioStation(); //фактический
+        assertEquals(expected, actual);
+    }
+
+    @Test //граничные значения 30
+    public void shouldTenRadioStationArgs() {
+        Radio rad = new Radio(30); //создалиРадиоСТекущейРадиостанцией 0
+        rad.setCurrentRadioStation(30);
+        int expected = 0; //ожидаемый
+        int actual = rad.getCurrentRadioStation(); //фактический
+        assertEquals(expected, actual);
+    }
+
+    @Test //кнопка next1(2)
+    public void shouldNextRadioStationArgs() {
+        Radio rad = new Radio(30); //создалиРадиоСТекущейРадиостанцией 0
+        rad.setCurrentRadioStation(1);
+        rad.nextRadioStation();
+        int expected = 2; //ожидаемый
+        int actual = rad.getCurrentRadioStation(); //фактический
+        assertEquals(expected, actual);
+    }
+
+    @Test //кнопка next3(4)
+    public void shouldNextRadioStation3Args() {
+        Radio rad = new Radio(30); //создалиРадиоСТекущейРадиостанцией 0
+        rad.setCurrentRadioStation(3);
+        rad.nextRadioStation();
+        int expected = 4; //ожидаемый
+        int actual = rad.getCurrentRadioStation(); //фактический
+
+        assertEquals(expected, actual);
+    }
+
+    @Test //кнопка next0(1)
+    public void shouldNextRadioStation0Args() {
+        Radio rad = new Radio(30); //создалиРадиоСТекущейРадиостанцией 0
+        rad.setCurrentRadioStation(0);
+        rad.nextRadioStation();
+        int expected = 1; //ожидаемый
+        int actual = rad.getCurrentRadioStation(); //фактический
+        assertEquals(expected, actual);
+    }
+
+    @Test //кнопка next8(9)
+    public void shouldNextRadioStation8Args() {
+        Radio rad = new Radio(30); //создалиРадиоСТекущейРадиостанцией 0
+        rad.setCurrentRadioStation(8);
+        rad.nextRadioStation();
+        int expected = 9; //ожидаемый
+        int actual = rad.getCurrentRadioStation(); //фактический
+        assertEquals(expected, actual);
+    }
+
+    @Test //кнопка next29(0)
+    public void shouldNextRadioStation29Args() {
+        Radio rad = new Radio(30); //создалиРадиоСТекущейРадиостанцией 0
+        rad.setCurrentRadioStation(29);
+        rad.nextRadioStation();
+        int expected = 0; //ожидаемый
+        int actual = rad.getCurrentRadioStation(); //фактический
+
+        assertEquals(expected, actual);
+    }
+
+    @Test //кнопка prev4(3)
+    public void shouldPrevRadioStation4Args() {
+        Radio rad = new Radio(30); //создалиРадиоСТекущейРадиостанцией 0
+        rad.setCurrentRadioStation(4);
+        rad.prevRadioStation();
+        int expected = 3; //ожидаемый
+        int actual = rad.getCurrentRadioStation(); //фактический
+        assertEquals(expected, actual);
+    }
+
+    @Test //кнопка prev0(9)
+    public void shouldPrevRadioStation0Args() {
+        Radio rad = new Radio(30); //создалиРадиоСТекущейРадиостанцией 0
+        rad.setCurrentRadioStation(0);
+        rad.prevRadioStation();
+        int expected = 29; //ожидаемый
+        int actual = rad.getCurrentRadioStation(); //фактический
 
         assertEquals(expected, actual);
     }
 
 
-    @Test //MaxVolum
-    public void shouldMaxVolum() {
-        Radio rad = new Radio();
-
-        rad.setToMaxVolume();
-
-        int expected = 10; //ожидаемый
-        int actual = rad.getCurrentVolume(); //фактический
-
-        assertEquals(expected, actual);
-    }
-
-    @Test //MinVolum
-    public void shouldMinVolum1() {
-        Radio rad = new Radio();
-
-        rad.setToMinVolume();
-
-
+    @Test //выставление уровня звука выше макс
+    public void shouldAboveTheMaxVolumeArgs() {
+        Radio rad = new Radio(30); //создалиРадиоСТекущейРадиостанцией 0
+        rad.setCurrentVolume(150);
         int expected = 0; //ожидаемый
         int actual = rad.getCurrentVolume(); //фактический
-
         assertEquals(expected, actual);
     }
+
+    @Test //выставление допустимого уровня звука
+    public void shouldCurrentVolumeArgs() {
+        Radio rad = new Radio(30); //создалиРадиоСТекущейРадиостанцией 0
+        rad.setCurrentVolume(100);
+        int expected = 100; //ожидаемый
+        int actual = rad.getCurrentVolume(); //фактический
+        assertEquals(expected, actual);
+    }
+
+    @Test //выставление уровня звука ниже мин
+    public void shouldBelowTheMinVolumeArgs() {
+        Radio rad = new Radio(30); //
+        rad.setCurrentVolume(-5);
+        int expected = 0; //ожидаемый
+        int actual = rad.getCurrentVolume(); //фактический
+        assertEquals(expected, actual);
+    }
+
+    @Test //граничные значения -1
+    public void shouldMinusOneVolumeArgs() {
+        Radio rad = new Radio(30); //создалиРадиоСТекущейРадиостанцией 0
+        rad.setCurrentVolume(-1);
+        int expected = 0; //ожидаемый
+        int actual = rad.getCurrentVolume(); //фактический
+        assertEquals(expected, actual);
+    }
+
+    @Test //граничные значения 0
+    public void shouldZeroVolumeArgs() {
+        Radio rad = new Radio(30); //создалиРадиоСТекущейРадиостанцией 0
+        rad.setCurrentVolume(0);
+        int expected = 0; //ожидаемый
+        int actual = rad.getCurrentVolume(); //фактический
+        assertEquals(expected, actual);
+    }
+
+    @Test //граничные значения 1
+    public void shouldOneVolumArgs() {
+        Radio rad = new Radio(30); //создалиРадиоСТекущейРадиостанцией 0
+        rad.setCurrentVolume(1);
+        int expected = 1; //ожидаемый
+        int actual = rad.getCurrentVolume(); //фактический
+        assertEquals(expected, actual);
+    }
+
+    @Test //граничные значения 99
+    public void shouldNineVolumArgs() {
+        Radio rad = new Radio(30); //создалиРадиоСТекущейРадиостанцией 0
+        rad.setCurrentVolume(99);
+        int expected = 99; //ожидаемый
+        int actual = rad.getCurrentVolume(); //фактический
+        assertEquals(expected, actual);
+    }
+
 }
